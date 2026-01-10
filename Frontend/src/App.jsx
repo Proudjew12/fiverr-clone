11e917d29ff7546048d44eb8cf5ce902b8da7e54
Ofek Cohen <ofek074@gmail.com>
2026-01-10 13:01:31 +0200

    Render information about the gig

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -4,6 +4,7 @@ import { HomePage } from './pages/HomePage.jsx'
 import { SearchResultsPage } from './pages/SearchResultsPage.jsx'
 import { AboutPage } from './pages/AboutPage.jsx'
 import { GigIndex } from './pages/GigIndex.jsx'
+import { GigDetails } from './pages/GigDetails.jsx'
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
           <Route path="/index" element={<GigIndex />} />
           <Route path="/search" element={<SearchResultsPage />} />
           <Route path="/about" element={<AboutPage />} />
+          <Route path='/gig/:gigId' element={<GigDetails/>}/>
         </Route>
       </Routes>
     </BrowserRouter>
