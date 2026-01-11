fa87fa5e975a389adecdcb0ad7be9a0634f11818
liaddj <liad.7735@gmail.com>
2026-01-11 17:02:18 +0200

    Add css to the filter

--- Frontend/src/components/filter/FilterDropDown.jsx
+++ Frontend/src/components/filter/FilterDropDown.jsx
@@ -1,13 +1,43 @@
 
 
 
-export function FilterDropDown({FilterName}) {
+export function FilterDropDown({ filterName }) {
 
-    return(
-    <div className="dropdown-container">
-      
-     
+  return (
 
-    </div>
+    <>
+    {filterName === "Seller details" && (
+     <div className="dropdown-container">
+
+      <h2 className="drop-name">{filterName}</h2>
+
+      <div className="checks-container">
+        <label className="check-filter">
+          <input type="checkbox" style={{ width: '18px', height: '18px', cursor: 'pointer' }} />
+          <span style={{ fontWeight: 'bold' }}>Top Rated Seller</span>
+        </label>
+
+        <label className="check-filter">
+          <input type="checkbox" style={{ width: '18px', height: '18px', cursor: 'pointer' }} />
+        <span style={{ fontWeight: 'bold' }}>Level 2</span>
+        </label>
+      </div>
+
+      <div className="checks-container">
+
+        <label className="check-filter">
+          <input type="checkbox" style={{ width: '18px', height: '18px', cursor: 'pointer' }} />
+          <span style={{ fontWeight: 'bold' }}>Level 1</span>
+        </label>
+
+        <label className="check-filter">
+          <input type="checkbox" style={{ width: '18px', height: '18px', cursor: 'pointer' }} />
+          <span style={{ fontWeight: 'bold' }}>New seller</span>
+        </label>
+
+      </div>
+    </div> 
+    )}
+    </>
   )
 }
